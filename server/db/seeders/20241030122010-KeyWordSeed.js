1c89b8f9f4118144f474f89f78bc4238d869570c
kex121 <t1mursobolev@yandex.ru>
2024-10-30 19:28:50 +0700

    add DB

---
+++ server/db/seeders/20241030122010-KeyWordSeed.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'KeyWords',
+      [
+        {
+          name: 'Кошка',
+          isGood: true,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Животные',
+          isGood: true,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'стрельба',
+          isGood: false,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'стрельба',
+          isGood: false,
+          userId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('KeyWords', null, {});
+  },
+};
