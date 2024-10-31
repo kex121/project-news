9850b3cb5a19f02ffd4ee76267a5621f2837f80e
kex121 <t1mursobolev@yandex.ru>
2024-10-31 20:48:36 +0700

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
+import Root from './components/ui/Root';
 import SearchPage from "./components/pages/SearchPage";
 import ProfilePage from './components/pages/ProfilePage';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { patch } from "@mui/material";
 
 function App() {
   const routes = [
     {
-      path: "/SearchPage",
-      element: <SearchPage />,
-    },
-    {
-      path: "/profile",
-      element: <ProfilePage />,
-    },
+      path: '/',
+      element: <Root/>,
+      children: [
+        {
+          path: "/",
+          element: <SearchPage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ]
+    }
   ];
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
---
+++ client/src/components/ui/NavBar.jsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function NavBar() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Логотип */}
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          {/* Профиль */}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Аватар" src="profile-user.png" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar;
